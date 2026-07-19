db83b03c10eb67baff7505c9f5e701583b498c70
onfarooq0008-eng <onfarooq0008@gmail.com>
2026-07-20 00:01:40 +0400

    Update headers.js

--- proxy/headers.js
+++ proxy/headers.js
@@ -3,13 +3,13 @@ function cleanHeaders(){
 return {
 
 "User-Agent":
-"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 Chrome/120 Safari/537.36",
+"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/126 Safari/537.36",
 
 "Accept":
-"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
 
-"Accept-Language":
-"en-US,en;q=0.9",
+"Accept-Encoding":
+"gzip, deflate, br"
 
 "Cache-Control":
 "no-cache"
